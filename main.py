dd38669f0bce9dec00a21962f3e6bb0cda122271
atrox3d <atrox3d@gmail.com>
2020-03-13 12:11:20 +0100

    prototype

---
+++ main.py
@@ -0,0 +1,70 @@
+import paramiko
+import time
+
+sshclient = paramiko.SSHClient()
+sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+hostname = '***REMOVED***'
+username = '***REMOVED***'
+password = '***REMOVED***'
+banner_timeout = 10
+
+print(f'connecting to {hostname} as {username}/{password}...')
+try:
+    sshclient.connect(
+        hostname=hostname,
+        username=username,
+        password=password,
+        # banner_timeout=banner_timeout
+    )
+
+    print(f'connected to {hostname}')
+
+    print('invoking shell...')
+    remote = sshclient.invoke_shell()
+    print(f'shell invoked: {remote}')
+
+    print('waiting for output...')
+    time.sleep(5)
+
+    print('receiving output...')
+    output = remote.recv(1000)
+    print(output.decode('utf-8'))
+
+    print('sending ?\\n...')
+    remote.send('?\n')
+
+    print('waiting for output...')
+    time.sleep(5)
+
+    print('receiving output...')
+    output = remote.recv(1000)
+    print(output.decode('utf-8'))
+
+    print('sending exit\\n...')
+    remote.send('exit\n')
+
+    print('waiting for output...')
+    time.sleep(5)
+
+    print('receiving output...')
+    output = remote.recv(1000)
+    print(output.decode('utf-8'))
+
+except Exception as e:
+    print(e)
+
+# sshsession = sshclient.get_transport().open_session()
+# print(sshsession)
+#
+# if sshsession.active:
+#     print('active')
+
+
+
+# stdin, stdout, stderr = sshclient.exec_command('\n\n\n\nhelp')
+# print(stdout.readlines())
+
+print(f'disconnecting from {hostname}...')
+sshclient.close()
+print(f'disconnected from {hostname}')
